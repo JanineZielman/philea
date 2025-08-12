59340997eb1245c6cc3349b32e0348d5a58dee1a
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-08-12 09:48:43 +0200

    popups

--- src/slices/KeyTopics/index.tsx
+++ src/slices/KeyTopics/index.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+'use client'
+import { FC, useState } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
@@ -7,10 +8,12 @@ import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
  */
 export type KeyTopicsProps = SliceComponentProps<Content.KeyTopicsSlice>;
 
-/**
- * Component for "KeyTopics" Slices.
- */
 const KeyTopics: FC<KeyTopicsProps> = ({ slice }) => {
+  const [popupData, setPopupData] = useState<{
+    text: Content.KeyTopicsSliceDefaultPrimaryTopicsItem["text"] | null;
+    index: number | null;
+  }>({ text: null, index: null });
+
   return (
     <section
       data-slice-type={slice.slice_type}
@@ -21,14 +24,38 @@ const KeyTopics: FC<KeyTopicsProps> = ({ slice }) => {
       <div data-aos="fade-up">
         <PrismicRichText field={slice.primary.text} />
       </div>
+
       <div className="topics">
         {slice.primary.topics.map((item, i) => (
-          <div className="topic" key={`key${i}`}>
+          <div
+            className={`topic cursor-pointer ${i % 2 === 1 ? "alt-color" : ""}`} // alternating topic colors
+            key={`key${i}`}
+            onClick={() => setPopupData({ text: item.text, index: i })}
+          >
             <p>{item.title}</p>
-            {/* <PrismicRichText field={item.text} /> */}
           </div>
         ))}
       </div>
+
+      {popupData.text && (
+        <div
+          className={`popup-topics ${popupData.index !== null && popupData.index % 2 === 1 ? "alt-color" : ""}`}
+          onClick={() => setPopupData({ text: null, index: null })}
+        >
+          <div
+            className="wrapper"
+            onClick={(e) => e.stopPropagation()} // prevent closing when clicking inside
+          >
+            <div
+              onClick={() => setPopupData({ text: null, index: null })}
+              className="close"
+            >
+              X
+            </div>
+            <PrismicRichText field={popupData.text} />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
